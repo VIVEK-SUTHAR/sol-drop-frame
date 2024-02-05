fadf0f8af17b8262bc54f001b9bae12fd0d96571
Vivek Suthar <sutharvivek18@gmail.com>
2024-02-05 13:35:54 +0530

    chore: initial

---
+++ src/app/api/drop/route.ts
@@ -0,0 +1,56 @@
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+
+const SOLANA_RPC_URL = "https://api.devnet.solana.com";
+
+export async function POST(req: Request) {
+  const { untrustedData } = await req.json();
+  try {
+    const solAddress = untrustedData?.inputText;
+    if (!solAddress) {
+      return new Response("Invalid Sol Address", { status: 400 });
+    }
+
+    const body = {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "requestAirdrop",
+      params: [solAddress, 1000000000],
+    };
+
+    const response = await fetch(SOLANA_RPC_URL, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      return new Response("Failed to airdrop", { status: 400 });
+    }
+    return new Response(
+      `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width" />
+          <meta property="og:title" content="SolDrop Frame by devvivek" />
+          <meta property="fc:frame" content="vNext" />
+          <meta
+            property="fc:frame:button:1"
+            content="Airdrop Successful"
+          />
+          <meta
+            name="fc:frame:post_url"
+            content=""
+          />
+        </head>
+        <body>
+            <h1>Airdrop Successful</h1>
+        </body> 
+      </html>`,
+      { headers: { "Content-Type": "text/html" }, status: 200 }
+    );
+  } catch (error) {
+    return new Response("Failed to airdrop", { status: 400 });
+  }
+}
