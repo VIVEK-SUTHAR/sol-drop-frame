2d4271df141047ef512f08037a1e7916e88d0d5b
Vivek Suthar <sutharvivek18@gmail.com>
2024-02-05 23:26:40 +0530

    chore: move to utils

--- src/app/api/drop/route.ts
+++ src/app/api/drop/route.ts
@@ -1,47 +1,27 @@
+import getFrameHTML from "@/app/utils/getFrameHTML";
 import { PublicKey } from "@solana/web3.js";
 
 const SOLANA_RPC_URL = "https://api.devnet.solana.com";
 
-const ERROR_HTML = `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
-    <meta property="og:title" content="SolDrop Frame by devvivek" />
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://sol-drop-frame.vercel.app/error.jpg" />
-    <meta
-    property="og:image"
-    content="https://sol-drop-frame.vercel.app/error.jpg"
-  />
-    <meta
-      property="fc:frame:button:1"
-      content="Retry Airdrop"
-    />
-    <meta
-      name="fc:frame:post_url"
-      content="https://sol-drop-frame.vercel.app"
-    />
-  </head>
-  <body>
-      <h1>Looks like something went wrong</h1>
-  </body> 
-</html>`;
-
+//1 Sol in Lamports
+const AIRDROP_AMOUNT = 1000000000;
 export async function POST(req: Request) {
   const { untrustedData } = await req.json();
   try {
     const solAddress = untrustedData?.inputText;
-
-    const solAddressValid = PublicKey.isOnCurve(solAddress);
-
+    const isSolAddressValid = PublicKey.isOnCurve(solAddress);
+    if (!isSolAddressValid) {
+      return new Response(getFrameHTML("invalid"), {
+        headers: { "Content-Type": "text/html" },
+        status: 200,
+      });
+    }
     const body = {
       jsonrpc: "2.0",
       id: 1,
       method: "requestAirdrop",
-      params: [solAddress, 1000000000],
+      params: [solAddress, AIRDROP_AMOUNT],
     };
-
     const response = await fetch(SOLANA_RPC_URL, {
       method: "POST",
       body: JSON.stringify(body),
@@ -49,73 +29,26 @@ export async function POST(req: Request) {
         "Content-Type": "application/json",
       },
     });
-    //Error handling
     if (!response.ok) {
-      return new Response(ERROR_HTML, {
+      return new Response(getFrameHTML("error"), {
         headers: { "Content-Type": "text/html" },
         status: 200,
       });
     }
-    return new Response(
-      `<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width" />
-          <meta property="og:title" content="SolDrop Frame by devvivek" />
-          <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://sol-drop-frame.vercel.app/success.jpg" />
-          <meta
-          property="og:image"
-          content="https://sol-drop-frame.vercel.app/success.jpg"
-        />
-          <meta
-            property="fc:frame:button:1"
-            content="Airdrop Successful"
-          />
-          <meta
-            name="fc:frame:post_url"
-            content=""
-          />
-        </head>
-        <body>
-            <h1>Airdrop Successful</h1>
-        </body> 
-      </html>`,
-      { headers: { "Content-Type": "text/html" }, status: 200 }
-    );
+    return new Response(getFrameHTML("success"), {
+      headers: { "Content-Type": "text/html" },
+      status: 200,
+    });
   } catch (error) {
-    console.log("error", error);
     if (error instanceof Error) {
       if (error.message.includes("Invalid")) {
-        return new Response(
-          `<!DOCTYPE html>
-          <html>
-            <head>
-              <meta charset="utf-8" />
-              <meta name="viewport" content="width=device-width" />
-              <meta property="og:title" content="SolDrop Frame by devvivek" />
-              <meta property="fc:frame" content="vNext" />
-              <meta property="fc:frame:image" content="https://sol-drop-frame.vercel.app/invalid.jpg" />
-              <meta
-              property="og:image"
-              content="https://sol-drop-frame.vercel.app/invalid.jpg"
-            />
-              <meta
-                property="fc:frame:button:1"
-                content="Retry With Valid Address"
-              />
-              <meta name="fc:frame:post_url" content="https://sol-drop-frame.vercel.app" />
-            </head>
-            <body>
-                <h1>Provide valid address</h1>
-            </body> 
-          </html>`,
-          { headers: { "Content-Type": "text/html" }, status: 200 }
-        );
+        return new Response(getFrameHTML("invalid"), {
+          headers: { "Content-Type": "text/html" },
+          status: 200,
+        });
       }
     }
-    return new Response(ERROR_HTML, {
+    return new Response(getFrameHTML("error"), {
       headers: { "Content-Type": "text/html" },
       status: 200,
     });
---
+++ src/app/utils/getFrameHTML.ts
@@ -0,0 +1,87 @@
+export default function getFrameHTML(
+  frameType: "success" | "invalid" | "error"
+) {
+  switch (frameType) {
+    case "success":
+      return SUCCESS_HTML;
+    case "error":
+      return ERROR_HTML;
+    case "invalid":
+      return INVALID_ADDRESS_HTML;
+  }
+}
+
+const ERROR_HTML = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta property="og:title" content="SolDrop Frame by devvivek" />
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="https://sol-drop-frame.vercel.app/error.jpg" />
+    <meta
+    property="og:image"
+    content="https://sol-drop-frame.vercel.app/error.jpg"
+  />
+    <meta
+      property="fc:frame:button:1"
+      content="Retry Airdrop"
+    />
+    <meta
+      name="fc:frame:post_url"
+      content="https://sol-drop-frame.vercel.app"
+    />
+  </head>
+  <body>
+      <h1>Looks like something went wrong</h1>
+  </body> 
+</html>`;
+
+const SUCCESS_HTML = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta property="og:title" content="SolDrop Frame by devvivek" />
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="https://sol-drop-frame.vercel.app/success.jpg" />
+    <meta
+    property="og:image"
+    content="https://sol-drop-frame.vercel.app/success.jpg"
+  />
+    <meta
+      property="fc:frame:button:1"
+      content="Airdrop Successful"
+    />
+    <meta
+      name="fc:frame:post_url"
+      content=""
+    />
+  </head>
+  <body>
+      <h1>Airdrop Successful</h1>
+  </body> 
+</html>`;
+
+const INVALID_ADDRESS_HTML = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta property="og:title" content="SolDrop Frame by devvivek" />
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="https://sol-drop-frame.vercel.app/invalid.jpg" />
+    <meta
+    property="og:image"
+    content="https://sol-drop-frame.vercel.app/invalid.jpg"
+  />
+    <meta
+      property="fc:frame:button:1"
+      content="Retry With Valid Address"
+    />
+    <meta name="fc:frame:post_url" content="https://sol-drop-frame.vercel.app" />
+  </head>
+  <body>
+      <h1>Provide valid address</h1>
+  </body> 
+</html>`;
